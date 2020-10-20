56e7cf8d7b734ab7c81fa073ee74954237fcef0f
J <J@afshans-MacBook-Pro.local>
2020-10-19 18:14:38 -0700

    testing routes

--- src/App.js
+++ src/App.js
@@ -12,13 +12,28 @@ class App extends Component {
     return (      
        <BrowserRouter>
         <div>
-          <Navigation />
+          {/* <Navigation />
             <Switch>
              <Route path="/" component={Home} exact/>
              <Route path="/about" component={About}/>
              <Route path="/contact" component={Contact}/>
             <Route component={Error}/>
-           </Switch>
+           </Switch> */}
+           <nav style={{ margin: 10 }}>
+					<NavLink to='/' exact activeClassName='active'>
+						Home
+					</NavLink>
+					<NavLink to='/about' activeClassName='active'>
+						About
+					</NavLink>
+					<NavLink to='/posts' activeClassName='active'>
+						Posts
+					</NavLink>
+				</nav>
+				<Route path='/' exact component={Home} />
+				<Route path='/about' component={About} />
+				<Route path='/posts' exact component={Posts} />
+				<Route path='/posts/:postId' exact component={Posts} />
         </div> 
       </BrowserRouter>
     );
